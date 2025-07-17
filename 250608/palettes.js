da3a846272988870c2d58417b3ca6242fdc2aa3a
udondon1478 <udon1478don@gmail.com>
2025-07-17 18:50:37 +0900

    ref: ダークモードとライトモード以外のカラーパレットを削除 :recycle:

--- 250608/palettes.js
+++ 250608/palettes.js
@@ -1,45 +1,5 @@
 const palettes = [
     {
-        bg: '#221e36',
-        colors: ['#dc5132', '#7a82b8', '#8ad0a6', '#c4f0a8', '#a0bb07', '#ffcf33', '#ec9f05'],
-        name: 'autmn-0',
-    },
-    {
-        bg: '#320e3b',
-        colors: ['#a2faa3', '#92c9b1', '#ffe5d9'],
-        name: 'bubbles-0',
-    },
-    {
-        bg: '#0A0103',
-        colors: ['#a2faa3', '#4f759b', '#ffe5d9'],
-        name: 'bubbles-1',
-    },
-    {
-        bg: '#180a3b',
-        colors: ['#477f8d', '#59babc', '#f5f5f5', '#d49add', '#ffaf00'],
-        name: 'pearly-0',
-    },
-    {
-        bg: '#232020',
-        colors: ['#f9c80e', '#f86624', '#ea3546', '#43bccd'],
-        name: 'solarFlair-0',
-    },
-    {
-        bg: '#190506',
-        colors: ['#0a71b6', '#f9c40a', '#eb5432', '#eaf2f0'],
-        name: 'squiggles-0',
-    },
-    {
-        bg: '#291720',
-        colors: ['#f75c03', '#e5beed', '#04a777'],
-        name: 'toyish-0',
-    },
-    {
-        bg: '#091540',
-        colors: ['#bba0ca', '#fff8e8', '#fcd581'],
-        name: 'valen-0',
-    },
-    {
         bg: '#ffffff',
         colors: ['#333333', '#666666', '#999999', '#aaaaaa', '#bbbbbb', '#cccccc', '#dddddd'],
         name: 'light-theme',
